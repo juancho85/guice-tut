1905e27b56bcb88f7053da5cb11908c19f28dd6b
Juan David Sánchez <juancho85@gmail.com>
2019-12-14 19:07:59 +0100

    linked binding

---
+++ src/main/java/org/example/WinWordSpellCheckerImpl.java
@@ -0,0 +1,8 @@
+package org.example;
+
+class WinWordSpellCheckerImpl extends SpellCheckerImpl {
+    @Override
+    public void checkSpelling() {
+        System.out.println("Inside WinWordSpellCheckerImpl.checkSpelling." );
+    }
+}
