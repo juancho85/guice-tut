c1bbcfa2168914fdf8ee7dace76c61a8407f34e9
Juan David Sánchez <juancho85@gmail.com>
2019-12-14 19:30:31 +0100

    instance bindings

--- src/main/java/org/example/TextEditor.java
+++ src/main/java/org/example/TextEditor.java
@@ -1,15 +1,22 @@
 package org.example;
 
+import com.google.inject.name.Named;
+
 import javax.inject.Inject;
 
 class TextEditor {
     private SpellChecker spellChecker;
+    private String dbUrl;
 
     @Inject
-    public TextEditor(SpellChecker spellChecker) {
+    public TextEditor(SpellChecker spellChecker, @Named("JDBC") String dbUrl) {
         this.spellChecker = spellChecker;
+        this.dbUrl = dbUrl;
     }
     public void makeSpellCheck() {
         spellChecker.checkSpelling();
+    }
+    public void makeConnection() {
+        System.out.println(dbUrl);
     }
 }
--- src/main/java/org/example/TextEditorModule.java
+++ src/main/java/org/example/TextEditorModule.java
@@ -1,11 +1,16 @@
 package org.example;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 
 class TextEditorModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(SpellChecker.class).to(SpellCheckerImpl.class);
         bind(SpellCheckerImpl.class).to(WinWordSpellCheckerImpl.class);
+
+        // Constant binding
+        bind(String.class).annotatedWith(Names.named("JDBC")).toInstance("jdbc:mysql://localhost:5326/emp");
+
     }
 }
